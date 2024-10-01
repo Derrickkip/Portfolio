c0550f2987d7647abe45b85d0ab2c2e16c6d9f82
derrickkip <derrick.korir@gmail.com>
2024-10-01 11:23:24 +0300

    add build folder

---
+++ build/precache-manifest.ed224f3b6dc7471e744230e6fa3b89e4.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76cc5f835b04c06f31e1200cf9290952",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ed1e5d7f9a111102861d",
+    "url": "/portfolio/static/css/main.0d9e98fb.chunk.css"
+  },
+  {
+    "revision": "067beb9f2564322a478a",
+    "url": "/portfolio/static/js/2.d3b6e2a1.chunk.js"
+  },
+  {
+    "revision": "9ef29cf4785219e6cb30bf9e2569b4b7",
+    "url": "/portfolio/static/js/2.d3b6e2a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ed1e5d7f9a111102861d",
+    "url": "/portfolio/static/js/main.a1b75202.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "418708e7c8d613112cf7c8ea6f06712b",
+    "url": "/portfolio/static/media/Font Awesome 5 Pro Solid.418708e7.woff"
+  },
+  {
+    "revision": "40878d85989fd728da121cf85281e981",
+    "url": "/portfolio/static/media/SpaceMono-Bold.40878d85.ttf"
+  },
+  {
+    "revision": "96985f7a507afce5ab786569d2b2368f",
+    "url": "/portfolio/static/media/SpaceMono-Regular.96985f7a.ttf"
+  },
+  {
+    "revision": "6d29eb7fac4fb42280b02bb564bac525",
+    "url": "/portfolio/static/media/bricklanehome.6d29eb7f.png"
+  },
+  {
+    "revision": "5668799fb262a59f5a271895ccfe3794",
+    "url": "/portfolio/static/media/cucumber.5668799f.png"
+  },
+  {
+    "revision": "1c84753d95c3687cc335a312d7e42348",
+    "url": "/portfolio/static/media/derrick.1c84753d.jpg"
+  },
+  {
+    "revision": "71a20edd4b10d155a634d7efc6827532",
+    "url": "/portfolio/static/media/icons8-amazon-web-services.71a20edd.svg"
+  },
+  {
+    "revision": "ae78c3adeafc6c9fe6eb476247a0cef0",
+    "url": "/portfolio/static/media/icons8-apollo.ae78c3ad.svg"
+  },
+  {
+    "revision": "46dcb4639a0664a65526b7d348bfc231",
+    "url": "/portfolio/static/media/icons8-docker.46dcb463.svg"
+  },
+  {
+    "revision": "68fe4fb0ebd4335de8e49ccee72db34d",
+    "url": "/portfolio/static/media/icons8-git.68fe4fb0.svg"
+  },
+  {
+    "revision": "102adadc1dd7fce20217da99e0f85542",
+    "url": "/portfolio/static/media/icons8-graphql.102adadc.svg"
+  },
+  {
+    "revision": "f6967c81822f43ac6ae2fbd1b915c17e",
+    "url": "/portfolio/static/media/icons8-javascript.f6967c81.svg"
+  },
+  {
+    "revision": "127d27006ac967663e54efb5582027d7",
+    "url": "/portfolio/static/media/icons8-magento.127d2700.svg"
+  },
+  {
+    "revision": "0c86da8850937ea6b0ef4dfcc5526f04",
+    "url": "/portfolio/static/media/icons8-postgresql.0c86da88.svg"
+  },
+  {
+    "revision": "0b948ab5dd3f9e988cd9130da860408f",
+    "url": "/portfolio/static/media/icons8-python.0b948ab5.svg"
+  },
+  {
+    "revision": "a248a9913b70873c61db8720301a651e",
+    "url": "/portfolio/static/media/icons8-redux.a248a991.svg"
+  },
+  {
+    "revision": "265f332738c918914d039db95271af46",
+    "url": "/portfolio/static/media/icons8-typescript.265f3327.svg"
+  },
+  {
+    "revision": "cf74446af42a0144d6d7f095e24c3e49",
+    "url": "/portfolio/static/media/kingdom.cf74446a.png"
+  }
+]);
